4a53ec383863c1122424b14dccdabd828672483a
Jonathan Hill <jwhill1452@gmail.comm>
2024-09-17 18:20:00 -0500

    Init. It has an inti now.

--- HandwritingDetector.py
+++ HandwritingDetector.py
@@ -9,3 +9,7 @@ print(trainingSet[0][0])
 pixelValues = list(trainingSet[0][0].getdata())
 print(pixelValues)
 print(len(pixelValues))
+
+#Should return an array with all the information of the network.
+def initializeNetwork(TESTSET, TRAINSET, numberOfPixels, numberOfLayer2Nodes, numberOfLayer3Nodes, numberOfLayer4Nodes):
+    network = []
